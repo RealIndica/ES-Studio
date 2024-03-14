ffd75c182b1ccb0df8dfd0979e5ed0c1b2faec40
BSRevival <dblegendsmud@gmail.com>
2024-03-14 11:37:44 -0700

    Refactor namespaces in ColorMapEditor project
    The namespaces in the files of the ColorMapEditor project have been updated for better organization and readability. The specific changes include moving FrmMain and ColorHeatMap to the Forms and Classes namespaces, respectively, while also introducing necessary 'using' directives.

--- ColorMapEditor/Controls/Table3DView.cs
+++ ColorMapEditor/Controls/Table3DView.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
+using ColorMapEditor.Classes;
 
 namespace ColorMapEditor.Controls
 {
--- ColorMapEditor/Forms/FrmMain.Designer.cs
+++ ColorMapEditor/Forms/FrmMain.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ColorMapEditor
+﻿namespace ColorMapEditor.Forms
 {
     partial class FrmMain
     {
